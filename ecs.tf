a1bbb1380a571c0c855d004dbebb191f4f6d0983
robertomoutinho <robertomoutinho@gmail.com>
2022-03-17 13:30:40 -0300

    syntax fix

--- ecs.tf
+++ ecs.tf
@@ -137,10 +137,10 @@ resource "aws_ecs_task_definition" "app" {
   dynamic "volume" {
     for_each = var.ecs_volumes == null ? [] : toset(var.ecs_volumes)
     content {
-      name = volume.value
+      name = each.value.name
       efs_volume_configuration {
-        file_system_id = volume.value.efs_volume_configuration.file_system_id
-        root_directory = volume.value.efs_volume_configuration.root_directory
+        file_system_id = each.value.efs_volume_configuration.file_system_id
+        root_directory = each.value.efs_volume_configuration.root_directory
       }
     }
   }
